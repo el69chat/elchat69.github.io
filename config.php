ad838af0acaef2207d80efd5e748b1532fb2176b
el69chat <71226309+el69chat@users.noreply.github.com>
2020-09-13 12:09:36 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "El 69 chat";
+
+// Site URL
+$site_url = "http://localhost/script"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "6236572326325375"; // Your purchase code, don't give it to anyone. 
+?>
